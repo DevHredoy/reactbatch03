25c0c3121eb8909430700ce5464869bbdfaf750f
DevHredoy <hredoy.rafiul@gmail.com>
2024-06-20 18:05:35 +0600

    saved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from 'antd'
 
 const App = () => {
+ 
+ let [num,setNumber]=useState(10);
+
+  
+
   return (
-    <div>sokal a call dio,shuye porbo!!!</div>
+    <div>sokal a call dio,shuye porbo!!!
+<div>
+
+  <Button onClick={()=>{
+    
+    setNumber(--num);
+    console.log("clicked in minus button:",num)}}>
+    -
+  </Button>
+    {num}
+    <Button onClick={()=>{setNumber(++num);
+    console.log("clicked in minus button:",num)}}>+</Button>
+</div>
+
+    </div>
   )
 }
 
