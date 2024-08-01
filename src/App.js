99a94c71501e699da304286e1f6b228e8e6186e6
DevHredoy <rafiul.hredoy@technonext.com>
2024-08-01 19:25:24 +0600

    practiced

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
-
-import React, { useState } from 'react'
-import { Button } from 'antd'
+import { Button } from "antd";
+import React, { useState } from "react";
 
 const App = () => {
- 
- let [num,setNumber]=useState(10);
+  let [num, setNumber] = useState(10);
+
+  const nicks = ["mamu", "kakke", "major"];
+
+  // if condition 1 is correct the clg will show you are in an university but if it is not correct it will show you are in a bay
+
+  const flag = true;
+
+  flag
+    ? console.log("you are in an university")
+    : console.log("you are in bay");
+
+  //flag?console.log("you are in an university"):console.log("you are in bay");
 
 
+//&& operation
 
-const nicks=["mamu","kakke","major"];
+const a=12;
+const b=14;
+const c=30;
+console.log(b>a && c>b);
 
-
-const modNicks=nicks.map((name)=>{
-
-console.log("the name is before :",name);
-return(name+"san"
-)
- 
-})
-
-console.log("the new array is now :",modNicks)
 
   return (
-    <div>sokal a call dio,shuye porbo!!!
-<div>
-
-  <Button onClick={()=>{
-    
-    setNumber(--num);
-    console.log("clicked in minus button:",num)}}>
-    -
-  </Button>
-    {num}
-    <Button onClick={()=>{setNumber(++num);
-    console.log("clicked in minus button:",num)}}>+</Button>
-</div>
-
+    <div>
+      sokal a call dio,shuye porbo!!!
+      <div>
+        <Button
+          onClick={() => {
+            setNumber(--num);
+            console.log("clicked in minus button:", num);
+          }}
+        >
+          -
+        </Button>
+        {num}
+        <Button
+          onClick={() => {
+            setNumber(++num);
+            console.log("clicked in minus button:", num);
+          }}
+        >
+          +
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
