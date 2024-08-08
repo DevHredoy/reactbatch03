454004b311d0f28fd19d2c32b1e042295deda348
DevHredoy <rafiul.hredoy@technonext.com>
2024-08-08 18:01:54 +0600

    untill demo of difference between array and object

--- src/App.js
+++ src/App.js
@@ -5,22 +5,57 @@ const App = () => {
   // let joyFriend3 = "afnan";
   // let joyFriend4 = "hasib";
   // let joyFriend5 = "pritom";
+// console.log("joy friend 0 :", joyFriend0);
+ // console.log("joy friend 1:", joyFriend1);
+// console.log("joy friend 2:", joyFriend2);
+// console.log("joy friend 3:", joyFriend3);
+// console.log("joy friend 4:", joyFriend4);
+// console.log("joy friend 5:", joyFriend5);
 
-  // console.log("joy friend 0 :", joyFriend0);
-
-  // console.log("joy friend 1:", joyFriend1);
-
-  // console.log("joy friend 2:", joyFriend2);
-
-  // console.log("joy friend 3:", joyFriend3);
-
-  // console.log("joy friend 4:", joyFriend4);
-
-  // console.log("joy friend 5:", joyFriend5);
+  let joyVaibondhura = ["arif", "ansar", "sajid", "afnan", "hasib", "pritom"];
+for (let i = 0; i < joyVaiFriends.length; i++) {
+    console.log("joy vai friend no:", i, joyVaibondhura[i]); }
 
 
 
-let joyVaiFriends=["arif","ansar","sajid","afnan","hasib","pritom"];
+
+
+let arif={"name":"arif",
+        "age":21
+}
+;
+let ansar={"name":"ansar",
+  "age":22
+}
+
+
+let sajid={"name":"sajid",
+  "age":28
+}
+
+
+let afnan={"name":"afnan",
+  "age":29
+}
+
+let afnanCar="audi";
+
+let hasib={"name":"hasib",
+  "age":18
+}
+
+let pritom={"name":"pritom",
+  "age":18
+}
+
+let joyVaiFriends=[arif,ansar,sajid,afnan,hasib,pritom];
+
+
+
+
+
+
+
 
 
 
