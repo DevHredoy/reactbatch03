8dfb2c0003073542288a44d8a14aed6d3661f2cc
DevHredoy <rafiul.hredoy@technonext.com>
2024-08-08 15:43:05 +0600

    demo of object done

--- src/App.js
+++ src/App.js
@@ -1,99 +1,41 @@
-import { Button } from "antd";
-import React, { useState } from "react";
-
 const App = () => {
-  let [num, setNumber] = useState(10);
+  // about joy vai
+  let joyVaiName = "joy";
+  let joyVaiAge = 24;
+  let joyVaiPlace = "nikunja";
+  let joyVaiProfession = "software developer";
 
-  const catchFish = (net) => {
-    //console.log("net name is :", net);
+  let RoyVaiName = "joy";
+  let RoyVaiAge = 24;
+  let RoyVaiPlace = "nikunja";
+  let RoyVaiProfession = "software developer";
 
-    for (let i = 0; i < 2; i++) {
-      if ((i = 1)) {
-        var specialnet = "mangonet";
-      }
-    }
-    // console.log(
-    //   "accesing mangonet insdie function but out side of the for loop where it has been declared:",
-    //   specialnet
-    // );
+  // imagine there are 100 variables
+
+  const joyVaiProfile = {
+    name: "joy",
+    age: 24,
+    Place: "nikunja",
+    Profession: "software developer",
   };
-  catchFish("currentjal");
-  // console.log(
-  //   "accesing mango net from outside of the function , in global scope",
-  //   specialnet
-  // );
-  const nicks = ["mamu", "kakke", "major"];
 
-  const modNicks = nicks.map((name) => {
-    //console.log("the name is before :", name);
-    return name + "san";
-  });
+  const joyVaiProfileJson = {
+    "name": "joy",
+    "age": 24,
+    "Place": "nikunja",
+    "Profession": "software developer",
+  };
 
-  //console.log("the new array is now :", modNicks);
+  const RoyVaiProfile = {
+    name: "Roy",
+    age: 26,
+    Place: "shantinagar",
+    Profession: "junior software developer",
+  };
 
-  // string
+  console.log("joy vai name:", joyVaiProfile.name);
 
-  // let joyvai = "hamidur";
-  // var hredoy = "hredoy";
-
-  // const armanvai = "arman";
-
-  //console.log("joy vai,hredoy,armanvai:", joyvai, hredoy, armanvai);
-  //number
-  // let independenceYear = 2024;
-  // var languageYear = 1952;
-  // const blacknight = 1971;
-
-  //console.log("inde,lang,black:", independenceYear, languageYear, blacknight);
-  //undefined
-  let unde1;
-
-  //unde1 = "joy";
-  console.log("type of unde1", typeof unde1);
-  //const unde2 = undefined;
-  var unde3 = undefined;
-
-  console.log("all undes:", unde1, unde3);
-
-  let joysName = "joy";
-
-  console.log("joy's name type is ", typeof joysName);
-
-
-
-
-
-  
-  //boolean
-
-  //big integer.
-
-  //Symbol
-
-  return (
-    <div>
-      sokal a call dio,shuye porbo!!!
-      <div>
-        <Button
-          onClick={() => {
-            setNumber(--num);
-            //console.log("clicked in minus button:", num);
-          }}
-        >
-          -
-        </Button>
-        {num}
-        <Button
-          onClick={() => {
-            setNumber(++num);
-            //console.log("clicked in minus button:", num);
-          }}
-        >
-          +
-        </Button>
-      </div>
-    </div>
-  );
+  return;
 };
 
 export default App;
