72db2d4be0ff799c800b22b2d9cdd678dabd0c0c
DevHredoy <hredoy.rafiul@gmail.com>
2024-08-11 08:53:11 +0600

    progressed

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
 const App = () => {
-  // let joyFriend0 = "arif";
-  // let joyFriend1 = "ansar";
-  // let joyFriend2 = "sajid";
-  // let joyFriend3 = "afnan";
-  // let joyFriend4 = "hasib";
-  // let joyFriend5 = "pritom";
-  // console.log("joy friend 0 :", joyFriend0);
-  // console.log("joy friend 1:", joyFriend1);
-  // console.log("joy friend 2:", joyFriend2);
-  // console.log("joy friend 3:", joyFriend3);
-  // console.log("joy friend 4:", joyFriend4);
-  // console.log("joy friend 5:", joyFriend5);
+  
 
-  let joyVaibondhura = [
-    "arif",
-    "ansar",
-    90,
-    "sajid",
-    "afnan",
-    "hasib",
-    "pritom",
-    true,
-    undefined,
-    19938493409783904893843984938498394839849839489384999999999999999999999999999999999999930849238094821093840938204982309480238409839848928309482309840923840923804823094092389439999999999999849389384883n,
-  ];
+let symbolA=Symbol('A');
+let symbolB=Symbol('A');
 
-  for (let i = 0; i < joyVaibondhura.length; i++) {
-    console.log(
-      "joy vai friend no:",
-      i,
-      joyVaibondhura[i],
-      typeof joyVaibondhura[i]
-    );
-  }
+let number1=undefined;
+let number2=undefined;
+console.log("others  ",number1==number2);
 
-  // let arif = { name: "arif", age: 21 };
-  // let ansar = { name: "ansar", age: 22 };
 
-  // let sajid = { name: "sajid", age: 28 };
 
-  // let afnan = { name: "afnan", age: 29 };
-  // let afnanCar = "";
 
-  // let hasib = { name: "hasib", age: 18 };
+let hredoyNames=["rafiul","kaka","kallu"];
 
-  // let pritom = { name: "pritom", age: 18 };
+console.log("array type is :",typeof hredoyNames)
 
-  // let joyVaiFriends = [arif, ansar, sajid, afnan, hasib, pritom];
+
+let set=new Set([22,2,2]);
+console.log("set =",set,typeof set)
+
+let map=new Map()
+
+map.set(1,1);
+map.set(3,4)
+map.set("district","dhaka")
+console.log("map:",map)
+//console.log("check if a and b is equal or not",symbolA==symbolB)
+
+
+
+
+
+
+let a=10;
+
+let b=a;
+b=20;
+
+console.log("a : ,b:",a,b)
+
+
+
+let num = 50;
+
+function modifyNumber(n) {
+  n = n + 100;
+  return n;
+}
+
+let result = modifyNumber(num);
+
+console.log(num);    // What will this log?
+console.log(result); // What will this log?
 
   return;
+
 };
 
 export default App;
