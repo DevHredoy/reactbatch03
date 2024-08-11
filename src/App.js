53fde31a0007cfc8990c3c215e14ac7b6947fdbd
DevHredoy <rafiul.hredoy@technonext.com>
2024-08-11 10:43:25 +0600

    on progress

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 const App = () => {
-  // about joy vai
-  let joyVaiName = "joy";
-  let joyVaiAge = 24;
-  let joyVaiPlace = "nikunja";
-  let joyVaiProfession = "software developer";
-
-  let RoyVaiName = "joy";
-  let RoyVaiAge = 24;
-  let RoyVaiPlace = "nikunja";
-  let RoyVaiProfession = "software developer";
-
-  // imagine there are 100 variables
-
-  const joyVaiProfile = {
-    name: "joy",
-    age: 24,
-    Place: "nikunja",
-    Profession: "software developer",
-  };
-
-  const joyVaiProfileJson = {
-    "name": "joy",
-    "age": 24,
-    "Place": "nikunja",
-    "Profession": "software developer",
-  };
-
-  const RoyVaiProfile = {
-    name: "Roy",
-    age: 26,
-    Place: "shantinagar",
-    Profession: "junior software developer",
-  };
-
-  console.log("joy vai name:", joyVaiProfile.name);
-
-  return;
+  return {
+    <div>hello</div>
+    
+      };
+ 
 };
 
 export default App;
