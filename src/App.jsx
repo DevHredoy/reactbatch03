64fdc2be9c660635158d289e5dbbf06bf1719977
DevHredoy <rafiul.hredoy@technonext.com>
2024-08-11 15:56:40 +0600

    with onclick meta tag

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const App = () => {
+  console.log("hello");
+
+  const danger_button = document.getElementById("one");
+  console.log("danger button:", danger_button);
+  const clkActionOne = () => {
+    alert("one clicked");
+  };
+
+  // danger_button.addEventListener("click", clkActionOne);
+
+  return (
+    <div>
+      <button id="one" onClick={clkActionOne}>
+        l
+      </button>
+    </div>
+  );
+};
+
+export default App;
