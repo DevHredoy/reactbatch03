07cf5c9a03937d5884ea5d5fa41c0e2696ffe64e
DevHredoy <hredoy.rafiul@gmail.com>
2024-08-13 09:25:56 +0600

    on progress

--- src/App.js
+++ src/App.js
@@ -1,68 +1,27 @@
 const App = () => {
-  // let joyFriend0 = "arif";
-  // let joyFriend1 = "ansar";
-  // let joyFriend2 = "sajid";
-  // let joyFriend3 = "afnan";
-  // let joyFriend4 = "hasib";
-  // let joyFriend5 = "pritom";
-// console.log("joy friend 0 :", joyFriend0);
- // console.log("joy friend 1:", joyFriend1);
-// console.log("joy friend 2:", joyFriend2);
-// console.log("joy friend 3:", joyFriend3);
-// console.log("joy friend 4:", joyFriend4);
-// console.log("joy friend 5:", joyFriend5);
-
-  let joyVaibondhura = ["arif", "ansar", "sajid", "afnan", "hasib", "pritom"];
-for (let i = 0; i < joyVaiFriends.length; i++) {
-    console.log("joy vai friend no:", i, joyVaibondhura[i]); }
+ 
 
 
 
+const alertron=(e)=>{
+alert("oi pakhi");
+console.log("e:",e)
 
 
-let arif={"name":"arif",
-        "age":21
-}
-;
-let ansar={"name":"ansar",
-  "age":22
 }
 
 
-let sajid={"name":"sajid",
-  "age":28
-}
-
-
-let afnan={"name":"afnan",
-  "age":29
-}
-
-let afnanCar="audi";
-
-let hasib={"name":"hasib",
-  "age":18
-}
-
-let pritom={"name":"pritom",
-  "age":18
-}
-
-let joyVaiFriends=[arif,ansar,sajid,afnan,hasib,pritom];
 
 
 
+  return(
 
+<div>
 
+  <button onMouseOver={alertron}>action</button>
+</div>
 
-
-
-
-
-
-
-
-  return;
+  );
 };
 
 export default App;
