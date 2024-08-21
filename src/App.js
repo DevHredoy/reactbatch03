c9b28c9f7752b44ec37f3324bfcb070ebcf2873e
DevHredoy <rafiul.hredoy@technonext.com>
2024-08-21 14:24:12 +0600

    the demo of nan

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-<<<<<<< HEAD
-import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
-=======
-
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>sokal a call dio,shuye porbo!!!</div>
-  )
->>>>>>> 3486c75aedca516cef2b9b964e8e02bf5a96bdb5
-}
+  let mymessage = "11";
 
-export default App
+  let number = Number.parseInt(mymessage);
+
+  console.log("number:", number);
+
+  
+
+  return <div>sokal a call dio,shuye porbo!!!</div>;
+};
+
+export default App;
