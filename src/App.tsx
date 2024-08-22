a308f1b8f7d0cd4814ec8a9067f615a28709a3af
DevHredoy <rafiul.hredoy@technonext.com>
2024-08-22 11:15:33 +0600

    implemented typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
 import React from "react";
-
-const App = () => {
-  let beef = "beef";
-  let chicken=null;
-  let goat ;
+import { FC } from "react";
 
 
+interface AppProps {
+  chicken: string;
+  beef: string;
+  goat: number;
+}
 
+const App : FC<AppProps>=({
+  chicken,
+  beef,
+  goat,
+}) => {
+  beef = "beef";
+chicken = null;
+   goat=false;
 
-// in the whole chain js engine will look for one single first expression which will not be undefined or null
-// first it will look if chicken is undefined or null/not,
+  // in the whole chain js engine will look for one single first expression which will not be undefined or null
+  // first it will look if chicken is undefined or null/not,
 
-//in other word js engine will for that value which is not undefined or not null
-  
-// once the expected value is found the value will passed to the result variable(in this case)
+  //in other word js engine will for that value which is not undefined or not null
 
-// among the six falsy value in coalescing operator null and undefined is considered undesired value
-let result = chicken ?? goat??beef;
+  // once the expected value is found the value will passed to the result variable(in this case)
+
+  // among the six falsy value in coalescing operator null and undefined is considered undesired value
+  let result = chicken ?? goat ?? beef;
   console.log("result", result);
 
-// DEMO OF TERNARY OPERATOR:
-//unlike the coalescing operator ternary take into account all the falsy values
+  // DEMO OF TERNARY OPERATOR:
+  //unlike the coalescing operator ternary take into account all the falsy values
 
-let result2=chicken?beef:goat;
-// guess the value inside the result2
+  let result2 = chicken ? beef : goat;
+  // guess the value inside the result2
 
-console.log("value of result2",result2)
+  console.log("value of result2", result2);
 
+  //DEMO OF shortcircuit somehow it is related to
+  chicken = "chicken";
+  let result3 = chicken && goat;
+  //guess the answer
+  console.log("value of result 3 : ", result3);
 
-//DEMO OF shortcircuit somehow it is related to 
-chicken="chicken"
-let result3=chicken && goat;
-//guess the answer
-console.log("value of result 3 : ",result3)
+  // if there are multiple expression with this operator the result will be "a"
+  //a will be that value the left expression of which is true,a could contain a false value.
 
-// if there are multiple expression with this operator the result will be "a"
-//a will be that value the left expression of which is true,a could contain a false value.
-
+  goat = "pandu";
   return <div>sokal a call dio,shuye porbo!!!</div>;
 };
 
